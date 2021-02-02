430c821a582d22e1b8866573cbef886ae2dce166
Sanjay Acharekar <621998sma@gmail.com>
2021-02-02 16:07:04 +0530

    nodejs project first commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require("path");
+const hbs = require('hbs');
+const app = express();
+
+const port = process.env.port || 8080;
+
+//public static path
+
+// console.log(path.join(__dirname,"../public"))
+const static_path = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partials_path = path.join(__dirname,"../templates/partials");
+
+
+
+
+app.set('view engine','hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path)
+
+app.use(express.static(static_path));
+
+
+//routing
+app.get("/",(req, res)=>{
+    res.render("index");
+});
+
+app.get("/about",(req, res)=>{
+    res.render("about");
+});
+
+app.get("/weather",(req, res)=>{
+    res.render("weather");
+});
+
+
+app.get("*",(req, res)=>{
+    res.render("404error",{
+        errorcomment: 'opps!! page could not be found'
+    });
+});
+
+
+app.listen(port, ()=>{
+console.log(`listening...${port}`);
+})
+
